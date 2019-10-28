7b0c4df97685d7efb9040b7b71ac77d92d24d3a9
Emil Martinov <martinov.emil@gmail.com>
2019-10-28 18:11:53 +0100

    Cloud Developer experience added

--- src/components/WorkXP/WorkXP.svelte
+++ src/components/WorkXP/WorkXP.svelte
@@ -1,18 +1,18 @@
 <script>
   let workXP = [
     {
+      title: "Mentor for Cloud Developer, Udacity",
+      beginningDate: "June 2019",
+      endDate: '',
+      text:"Cloud Developer program is designed to take the student from basics like how to host a website on S3 with Cloudfront CDN, spinning up ElasticBeanstalk web app that scales by compute resource needs or traffic. The students also migrate a monolith app to a microservice architecture using AWS. Lastly, the students are guided to tap into the power of serverless."
+    },
+    {
       title: "Mentor for Frontend Web Development, Udacity",
       beginningDate: "May 2018",
       endDate: '',
-      // dates: "May 2018 - ",
       text:
         "Udacity is an online education platform which partners with companies like Google, Facebook, Github, Amazon and IBM to bring industry standard education programs to prepare candidates to the job market. I work as a reviewer and mentor to enable students grow into their job roles. I have reviewed hundreds of projects, with an average of 4.92 out of 5 as student feedback."
     }
-    // {
-    //   title: "Neighbourhood Project - https://bit.ly/2PiNPxx",
-    //   dates: "June 2016 - January 2017",
-    //   text: "Hello there"
-    // }
   ];
 </script>
 <style>
@@ -21,6 +21,6 @@
 <h2>Work Experience</h2>
 {#each workXP as work}
 <h2>{work.title}</h2>
-<p>{work.beginningDate} - {work.endDate ? work.endDate : 'now'}</p>
+<p>{work.beginningDate} - {work.endDate ? work.endDate : 'Present'}</p>
 <p>{work.text}</p>
 {/each}
