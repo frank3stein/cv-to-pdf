eb4066887e12cfedd9db832ca1eb1808a37bab21
Emil Martinov <martinov.emil@gmail.com>
2020-10-15 10:50:31 +0200

    Devops projects added

--- src/components/WorkXP/WorkXP.svelte
+++ src/components/WorkXP/WorkXP.svelte
@@ -20,16 +20,16 @@ li {
 <h2>{work.title}</h2>
 <p>{work.beginningDate} - {work.endDate ? work.endDate : 'Present'}</p>
 {/each}
-  <p>Udacity is an online education platform which partners with companies like Google, Facebook, Github, Amazon and IBM to bring industry standard education programs to prepare candidates for the job market. 
+  <p>Udacity is an online education platform that partners with companies like Google, Facebook, Github, Amazon, and IBM to bring industry-standard education programs to prepare candidates for the job market. 
     <ul>
       <li>
         Reviewed thousands of projects consistently rating high on performance metrics, <b>review rating (&gt;4.9/5)</b>, <b>avg project review turnaround time (&lt;1hour/8hours).</b>
      </li>
-     <li>Udacity saw an 260% increase in annual recurring revenue in the first half of 2020, in this unprecendent times I have welcomed new responsibilities. At the moment, I am responsible for 17 course modules across different degrees.
+     <li>Udacity saw a 260% increase in annual recurring revenue in the first half of 2020, at the height of increased workload, I not only fulfilled my duties but welcomed new ones. At the moment, I am responsible for 17-course modules across different degrees.
     </li>
     <li>
-      Resolved hundreds of issues on Javascript, React/Redux, AWS, through providing 1 to 1 guidance to students on their learning journey, teaching the what, how and why.
+      Resolved hundreds of issues on Javascript, React/Redux, AWS, by providing 1 to 1 guidance to students on their learning journey, teaching the what, how, and why.
     </li>
-    <li>Additional Skills: <strong>CI/CD (CircleCI, Travis), Microservices (Docker, Kubernetes), AWS(Lambda, S3, EC2, RDS, DynamoDB, IAM, VPC, SG)</strong></li>
+    <li>Additional Skills: <strong>CI/CD (CircleCI, Travis), Microservices (Docker, Kubernetes), AWS(Lambda, S3, EC2, RDS, DynamoDB, IAM, VPC, SG), Vue, Svelte (this CV)</strong></li>
   </ul>
   
